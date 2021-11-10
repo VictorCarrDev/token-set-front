ed85dc4475d7490e06af1c071b46141cba3372ee
Orisoth <viccarrillo@hotmail.com>
2021-11-09 23:54:55 -0400

    updated to last 80 days

--- src/components/grahc.js
+++ src/components/grahc.js
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 //1. Import coingecko-api
-const CoinGecko = require('coingecko-api');
+const CoinGecko = require("coingecko-api");
 
 //2. Initiate the CoinGecko API Client
 const CoinGeckoClient = new CoinGecko();
 
 //3. Make calls
-const getData = async () => {
-  const result = await CoinGeckoClient.coins.fetchMarketChartRange('bitcoin', { from: 1392577232, to: 1422577232 });
-  return result
+const getData = async (from, to) => {
+  const result = await CoinGeckoClient.coins.fetchMarketChartRange("bitcoin", {
+    from: from,
+    to: to,
+  });
+  return result;
 };
 
-
-
 const options = {
-  plugins: { legend: { display: false } },
-  scales: {
-    xAxes: {
-      display: false //this will remove all the x-axis grid lines
-    }
+  tooltips: {
+    callbacks: {
+      label: (item) => `${item.yLabel} $`,
+    },
   },
+  plugins: { legend: { display: false } },
+  // scales: {
+  //   xAxes: {
+  //     display: false //this will remove all the x-axis grid lines
+  //   }
+  // },
   elements: {
     point: {
       radius: 0,
@@ -28,11 +34,9 @@ const options = {
   },
 };
 
-
 const Chart = (props) => {
-
-  const [xValues, setXValues] = useState([])
-  const [yValues, setYValues] = useState([0, 10, 5, 2, 20, 30, 45])
+  const [xValues, setXValues] = useState([]);
+  const [yValues, setYValues] = useState([0, 10, 5, 2, 20, 30, 45]);
   const data = {
     labels: xValues,
     datasets: [
@@ -46,29 +50,36 @@ const Chart = (props) => {
   };
 
   useEffect(() => {
+    const range = (n) =>
+      [...Array(n).keys()].map((num) => {
+        const hour = (new Date().getHours()) - (n - num)
+        return new Date(
+          new Date().setHours(hour)
+        ).toDateString();
+      });
 
-    const range = n => [...Array(n).keys()]
+    let yData;
+    let xData;
+    const before = new Date(new Date().setDate(-80)) / 1000;
+    console.log(before);
+    const after = new Date() / 1000;
+    getData(before, after).then((value) => {
+      console.log(value.data.prices);
+      xData = range(value.data.prices.length);
+      yData = value.data.prices.map((cap) => {
+        return cap[1];
+      });
 
-    let yData
-    let xData
-    getData().then(value => {
-      console.log(value.data.market_caps)
-      xData = range(value.data.market_caps.length)
-      yData = value.data.market_caps.map((cap) => {
-        return cap[1]
-      })
-
-      setXValues(xData)
-      setYValues(yData)
-    })
-
-  }, [])
+      setXValues(xData);
+      setYValues(yData);
+    });
+  }, []);
 
   return (
     <div className=" w-6/12 mx-auto flex ">
       <Line data={data} options={options} />
     </div>
-  )
+  );
 };
 
 export default Chart;
